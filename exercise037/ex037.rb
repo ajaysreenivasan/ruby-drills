555b8249476207cbfd6f0f50d037ddcd46ced46d
Ajay Sreenivasan <ajaysreenivasan@gmail.com>
2016-05-25 20:45:21 -0400

    Is it over yet...

--- exercise037/ex037.rb
+++ exercise037/ex037.rb
@@ -1,1 +1,50 @@
 # Exercise 37 - Symbol Review
+
+# Keywords
+# BEGIN - run this block on script start : BEGIN { ... }
+# END - run this block on script end : END { ... }
+# alias - create another name for f'n : alias X Y
+# and - logical, but lower priority than && : "puts "Hello" and "Goodbye""
+# begin - start a block, usually for exceptions : begin/end
+# break - -_- break
+# case - case conditional : X; when Y; else; end
+# class - define new class : class X; end
+# def - define new f'n : def leedle(); end
+# defined? - is this f'n/class/etc defined already? : defined? Class == "constant"
+# do - create block that maybe takes param : (0..n).each do |x| puts x end
+# else - else conditional
+# elsif - elsif conditional
+# end - ends blocks, f'ns, classes, everything
+# ensure - run this code whether or not exception occurs : begin ensure end
+# for - for loop, .each is preferred : for X in Y; end
+# if - if conditional
+# in - part of for-loops
+# module - define new module
+# next - move to next element of .each iterator
+# not - logical not, use ! instead
+# or - logical or, use ||
+# redo - rerun block, exactly the same : (0..5).each { |i| redo if i > 2 }
+# rescue - run this code if exception happens
+# retry - in rescue clause, says to try block again
+# return - -_- sigh
+# self - current object, class, module : defined? self == "self"
+# super - parent class of this class
+# then - can be used with "if", optional : if ___ then ___
+# undef - remove function def from class : undef X
+# unless - inverse of if
+# until - inverse of while, execute while false
+# when - part of case conditionals
+# while - while loop
+# yield - pause/transfer control to code block
+
+
+# Data Types
+# true, false - -_-
+# nil - nothing/no value
+# strings - sigh -_-
+# numbers - integers
+# floats - zz
+# arrays - lists apparently
+# hashes - perks : e = {'x' => 1, 'y' => 2}
+
+
